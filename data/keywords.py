eb4e42e29149314192278a9735410fe40fd2a586
Leonid Chistyakov <79141656+LeonidChistyakov@users.noreply.github.com>
2021-12-09 12:56:29 +0700

    Added video demonstration

--- data/keywords.py
+++ data/keywords.py
@@ -4,12 +4,12 @@ keywords_en = {
     ("hello", "hi"): cmd.greetings,
     ("bye", "goodbye"): cmd.farewell,
     ("create", "new"): cmd.create_new_event,
-    ("main"): cmd.go_main,
+    ("password", ""): cmd.change_password,
+    ("main", ""): cmd.go_main,
     ("delete", "remove"): cmd.delete_event,
-    ("password"): cmd.change_password,
     ("yes", "accept"): cmd.yes,
     ("no", "decline"): cmd.no,
     ("back", "previous"): cmd.go_back,
-    ("next"): cmd.go_next,
-    ("writting"): cmd.writting
+    ("next", ""): cmd.go_next,
+    ("writting", ""): cmd.writting
 }
